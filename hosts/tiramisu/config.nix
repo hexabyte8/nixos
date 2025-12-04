01dc3aba7a317a8e65db5512ae05ce831558a019
installer <installer@gmail.com>
2025-12-04 08:50:04 -0600

    fix for typo in config.nix

--- hosts/tiramisu/config.nix
+++ hosts/tiramisu/config.nix
@@ -126,9 +126,14 @@ in
 
   # networking
   networking = {
+    nameserver = [ "8.8.8.8" "1.1.1.1" ];
     networkmanager.enable = true;
     hostName = "${host}";
     timeServers = options.networking.timeServers.default ++ [ "pool.ntp.org" ];
+  };
+
+  services.resolved = {
+    enable = true;
   };
 
 
@@ -343,16 +348,11 @@ in
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
-  networking.nameserver = [ "8.8.8.8" "1.1.1.1" ]
-    services.resolved = {
-  enable = true;
-};
-
-# This value determines the NixOS release from which the default
-# settings for stateful data, like file locations and database versions
-# on your system were taken. It‘s perfectly fine and recommended to leave
-# this value at the release version of the first install of this system.
-# Before changing this value read the documentation for this option
-# (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-system.stateVersion = "24.11"; # Did you read the comment?
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.11"; # Did you read the comment?
 }
